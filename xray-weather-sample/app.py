d76173db6e6659c31ebb3201e3c2983ac3f254e8
sajith <sajith.weerakoon@myob.com>
2018-05-30 17:40:25 +1000

    Adding initial code

---
+++ xray-weather-sample/app.py
@@ -0,0 +1,83 @@
+from chalice import Chalice
+from weather import Weather, Unit
+import requests
+import json
+import os
+import boto3
+from aws_xray_sdk.core import patch_all
+
+app = Chalice(app_name='xray-weather-sample')
+
+
+@app.route('/')
+def index():
+    patch_all()
+    try:
+        data = fetch_weather_data()
+        formatted_data = format_data(data)
+        update_database(json.loads(formatted_data)['data'])
+        notify_sns(json.loads(formatted_data)['data'])
+        return formatted_data
+    except Exception as e:
+        print(e)
+        return e
+
+
+@app.route('/trim', methods=['POST'], content_types=['application/x-www-form-urlencoded', 'application/json'])
+def trim_data():
+    patch_all()
+    try:
+        request = app.current_request
+        data = request.raw_body.decode("utf-8")
+        forecast_data_map = json.loads(data)
+        items_map = json.loads(forecast_data_map['data'])
+        forecast_data = []
+        for date, content in items_map.items():
+            content = {**content, 'date': date}
+            forecast_data.append(content)
+        return {'data': forecast_data}
+    except Exception as e:
+        print(e)
+
+
+def fetch_weather_data():
+    weather = Weather(unit=Unit.CELSIUS)
+    location = weather.lookup_by_location('sydney')
+    forecasts = location.forecast
+    output = {}
+    for forecast in forecasts:
+        forecast_data_map = {}
+        forecast_data_map['forecast'] = forecast.text
+        forecast_data_map['high'] = forecast.high
+        forecast_data_map['low'] = forecast.low
+        output[forecast.date] = forecast_data_map
+    return output
+
+
+def format_data(data):
+    headers = {'Content-type': 'application/x-www-form-urlencoded'}
+    response = requests.post('https://92qef2fmvh.execute-api.ap-southeast-2.amazonaws.com/api/trim',
+                             json={"data": json.dumps(data)}, headers=headers)
+    return response.text
+
+
+def update_database(data):
+    resource = boto3.resource('dynamodb')
+    table = resource.Table(os.environ['XRAY_DYNAMO_TABLE'])
+    table.delete
+    if isinstance(data, list):
+        for d in data:
+            table.put_item(
+                Item=d
+            )
+
+
+def notify_sns(data):
+    sns_client = boto3.client('sns')
+    sns_client.publish(
+        TargetArn=os.environ['XRAY_SNS_TOPIC'],
+        Message=json.dumps(
+            {'default': json.dumps(data)}),
+        MessageStructure='json'
+    )
+    print('notify_sns')
---
+++ xray-weather-sample/create_table.py
@@ -0,0 +1,75 @@
+import os
+import uuid
+import json
+import argparse
+
+import boto3
+
+
+TABLES = {
+    'xray': {
+        'prefix': 'xray-test',
+        'env_var': 'XRAY_DYNAMO_TABLE',
+        'date_key': 'date'
+    },
+}
+
+
+def create_table(table_config):
+    table_name = '%s-%s' % (table_config['prefix'], str(uuid.uuid4()))
+    client = boto3.client('dynamodb')
+    key_schema = [
+        {
+            'AttributeName': table_config['date_key'],
+            'KeyType': 'HASH',
+        }
+    ]
+    attribute_definitions = [
+        {
+            'AttributeName': table_config['date_key'],
+            'AttributeType': 'S',
+        }
+    ]
+    client.create_table(
+        TableName=table_name,
+        KeySchema=key_schema,
+        AttributeDefinitions=attribute_definitions,
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5,
+        }
+    )
+    waiter = client.get_waiter('table_exists')
+    waiter.wait(TableName=table_name, WaiterConfig={'Delay': 1})
+    return table_name
+
+
+def record_as_env_var(key, value, stage):
+    with open(os.path.join('.chalice', 'config.json')) as f:
+        data = json.load(f)
+        data['stages'].setdefault(stage, {}).setdefault(
+            'environment_variables', {}
+        )[key] = value
+    with open(os.path.join('.chalice', 'config.json'), 'w') as f:
+        serialized = json.dumps(data, indent=2, separators=(',', ': '))
+        f.write(serialized + '\n')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--stage', default='dev')
+    # app - stores the todo items
+    # users - stores the user data.
+    parser.add_argument('-t', '--table-type', default='xray',
+                        choices=['xray'],
+                        help='Specify which type to create')
+    args = parser.parse_args()
+    table_config = TABLES[args.table_type]
+    table_name = create_table(
+        table_config
+    )
+    record_as_env_var(table_config['env_var'], table_name, args.stage)
+
+
+if __name__ == '__main__':
+    main()
